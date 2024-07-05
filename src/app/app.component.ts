434d86b2c1b136092acff9cd4510969c6cac7d2f
Christoper Ramirez <19002167@alumnos.utleon.edu.mx>
2024-07-05 14:08:16 -0600

    nav sin ruta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsModule } from './products/products.module';
+import { LayoutModule } from './layout/layout.module';
+import { HomeModule } from './home/home.module';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsModule, LayoutModule, HomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductsComponent } from './products/products.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductsComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ProductsComponent
   ]
 })
 export class ProductsModule { }
